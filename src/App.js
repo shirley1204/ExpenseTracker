11010dffe6cb0cb8c42b1d2355084fe310272597
shirley1204 <you@example.com>
2020-10-08 04:26:23 -0800

    first time

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Header} from './components/Header'
+import {Balance} from './components/Balance'
+import {IncomeExpenses} from './components/IncomeExpenses'
+import TrasactionList from './components/TransactionList'
+import {AddTransaction} from './components/AddTransaction'
+
+import GlobalProvider from './context/GlobalState';
+
+import './App.css';
+
+function App() {
+  return (
+  <GlobalProvider>
+   <Header />
+   <div className="container">
+     <Balance />
+     <IncomeExpenses />
+     <TrasactionList />
+     <AddTransaction />
+   </div>
+   </GlobalProvider>
+   
+ );
+}
+
+export default App;
